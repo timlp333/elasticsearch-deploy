daa6e7662a48589882f279eb44d6bc118abbc456
timyu <timyu@softmobile.com.tw>
2024-08-20 16:50:22 +0800

    update step2.sh

--- step2.sh
+++ step2.sh
@@ -5,18 +5,15 @@ ELASTICSEARCH_CONFIG_PATH="/home/elastic/elasticsearch-8.15.0/config/elasticsear
 
 ELASTICSEARCH_VERSION=8.15.0
 
-tar -zxf /home/elastic/elasticsearch-$ELASTICSEARCH_VERSION/config/certs.tar.gz
+cd /home/elastic/elasticsearch-$ELASTICSEARCH_VERSION/config
+
+tar -zxf certs.tar.gz
 
 chown -R elastic:elastic /home/elastic/elasticsearch-$ELASTICSEARCH_VERSION/config/certs
 
 echo "Updated network.host to $LOCAL_IP in $ELASTICSEARCH_CONFIG_PATH"
 
 sed -i "s/^network.host: {localip}/network.host: $LOCAL_IP/" $ELASTICSEARCH_CONFIG_PATH
-
-
-
-
-##########################################################################################
 
 
 
