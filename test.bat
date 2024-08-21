df425eb0f78f7515872930a3b0fe158b2343a059
timyu <timyu@softmobile.com.tw>
2024-08-21 11:33:43 +0800

    bug fix

--- test.bat
+++ test.bat
@@ -3,8 +3,16 @@ chcp 65001
 setlocal enabledelayedexpansion
 set HOSTFILE=hosts.txt
 set COUNTER=1
-for /f %%i in (%HOSTFILE%) do (
-    echo %COUNTER%
-    set /a COUNTER+=1
+@REM for /f %%i in (%HOSTFILE%) do (
+@REM     echo %COUNTER%
+@REM     set /a COUNTER+=1
+@REM )
+
+:loop
+echo COUNTER is !COUNTER!
+set /a COUNTER+=1
+
+if !COUNTER! leq 3 (
+    goto loop
 )
 pause
